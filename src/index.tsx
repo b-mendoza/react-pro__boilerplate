f83ea39961014a8bb7cd42e2cd8a751ff592f01e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-11 00:12:34 -0600

    feat: update `index` file from `src` directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+
+import { Router } from 'components/Router';
+
+const rootNode = document.getElementById('root');
+
+render(
+  <StrictMode>
+    <Router />
+  </StrictMode>,
+  rootNode,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
