6d711fa636138219271538f35e239d4d1a0c4cd9
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-11 00:14:08 -0600

    feat: add `shared` styled components to `theme` directory

---
+++ src/theme/shared.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+import { paddingReset } from './resets';
+
+export const ContentWrapper = styled.section`
+  ${paddingReset}
+
+  width: 100%;
+`;
+
+export const MainLayout = styled.main`
+  display: flex;
+`;
