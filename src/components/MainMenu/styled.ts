eea38d3ebce970edd487b009382688f3a46a6287
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-11 00:16:34 -0600

    feat: add styled components of `MainMenu` component

---
+++ src/components/MainMenu/styled.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const StyledMenuItem = styled.li`
+  text-align: center;
+
+  &:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+`;
+
+export const StyledMenuWrapper = styled.aside`
+  background-color: #363a45;
+
+  height: 100vh;
+
+  width: 25rem;
+`;
